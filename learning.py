366f51adf7f6540d46f6c808c4ceb5d9243e37dc
TomasHalko <tomashalko@gmail.com>
2020-09-07 16:51:45 +0200

    last commit for the day

--- learning.py
+++ learning.py
@@ -1,7 +1,29 @@
+# First Exercise
+""" prompt = "> "
 print("What is your name?")
 name = input()
 
 print("What is your age?")
 age = input()
 
+print("Hi your name is "+ name + " and your age is " + age + ".") """
+
+# Second exercise
+"""prompt = "> "
+print("What is your name?")
+name = input()
+
+if (name == "Tomas"):
+    print("You are a god")
+else: print("ok boomer")
+
+print("What is your age?")
+age = input()
+
 print("Hi your name is "+ name + " and your age is " + age + ".")
+"""
+
+print("Whats your age?")
+age = int(input())
+
+if (age%10)
